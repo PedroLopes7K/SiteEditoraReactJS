abb7e64d6cd5d758d4d4c026c45c79afeb31c888
PedroLopes7K <pedrohenri3601@gmail.com>
2022-03-10 20:19:27 -0300

    Editora MaujorTec

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg'
+// import React, { Fragment } from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="topo">
+        <h1 className="logo"></h1>
+        <ul>
+          <li>
+            <a href="">Navegação</a>
+          </li>
+        </ul>
       </header>
-    </div>
-  );
+
+      <main className="principal">
+        <h2>Ultimos Lançamentos</h2>
+        <div className="card"> Card</div>
+      </main>
+      <footer className="rodape">
+        <p>Conteúdos cedido pela Editora Novatec&#8212; Copyright 2022</p>
+      </footer>
+    </>
+  )
 }
 
-export default App;
+export default App
