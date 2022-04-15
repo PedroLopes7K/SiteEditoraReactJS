2c6d4566756e96e5b4e252a458c75ac247491e9e
PedroLopes7K <pedrohenri3601@gmail.com>
2022-04-15 08:53:25 -0300

    Update 15/04

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Topo from './Components/Topo'
 import Home from './Components/Home'
@@ -10,74 +10,69 @@ import Notfound from './Components/Notfound'
 import Rodape from './Components/Rodape'
 import './index.css'
 import Livro from './Components/Livro'
-import axios from 'axios'
+// import axios from 'axios'
 
-class App extends Component {
-  state = {
-    livros: []
-  }
+function App() {
+  const [livros, setLivros] = useState([])
 
-  async componentDidMount() {
-    try {
-      const { data: livros } = await axios.get('/api/todosOsLivros.json')
-      console.log(livros)
-      this.setState({ livros })
-    } catch (error) {
-      console.log(error)
-      document
-        .querySelectorAll('.principal')[0]
-        .insertAdjacentHTML(
-          'beforeend',
-          "<p class 'erro' > Mensagem de erro </p>"
-        )
-    }
-  }
+  useEffect(() => {
+    fetch('/api/todosOsLivros.json')
+      .then(resposta => resposta.json())
+      .then(dados => {
+        console.log(dados)
+        setLivros(dados)
+      })
+  }, [])
 
-  render() {
-    return (
-      <Router>
-        <Topo />
+  // async componentDidMount() {
+  //   try {
+  //     const { data: livros } = await axios.get('/api/todosOsLivros.json')
+  //     console.log(livros)
+  //     this.setState({ livros })
+  //   } catch (error) {
+  //     console.log(error)
+  //     document
+  //       .querySelectorAll('.principal')[0]
+  //       .insertAdjacentHTML(
+  //         'beforeend',
+  //         "<p class 'erro' > Mensagem de erro </p>"
+  //       )
+  //   }
+  // }
 
-        <Routes>
-          <Route exact path="/" element={<Home livros={this.state.livros} />} />
-          <Route
-            exact
-            path="/frontend"
-            element={<Frontend livros={this.state.livros} />}
-          />
-          <Route
-            exact
-            path="/programacao"
-            element={<Programacao livros={this.state.livros} />}
-          />
-          <Route
-            exact
-            path="/design"
-            element={<Design livros={this.state.livros} />}
-          />
-          <Route
-            exact
-            path="/catalogo"
-            element={<Catalogo livros={this.state.livros} />}
-          />
-          <Route
-            exact
-            path="/livro/:slug"
-            element={<Livro />}
-            // element={props => {
-            //   const livro = this.state.livros.find(
-            //     livro => livro.slug === props.match.params.slug
-            //   )
-            //   if (livro) return <Livro livro={livro} />
-            //   else return <Notfound />
-            // }}
-          />
-          <Route component={Notfound} />
-        </Routes>
-        <Rodape />
-      </Router>
-    )
-  }
+  // render() {
+  return (
+    <Router>
+      <Topo />
+
+      <Routes>
+        <Route exact path="/" element={<Home livros={livros} />} />
+        <Route exact path="/frontend" element={<Frontend livros={livros} />} />
+        <Route
+          exact
+          path="/programacao"
+          element={<Programacao livros={livros} />}
+        />
+        <Route exact path="/design" element={<Design livros={livros} />} />
+        <Route exact path="/catalogo" element={<Catalogo livros={livros} />} />
+        <Route
+          exact
+          path="/livro/:slug"
+          element={<Livro />}
+          // element={props => {
+          //   const livro = this.state.livros.find(
+          //     livro => livro.slug === props.match.params.slug
+          //   )
+          //   if (livro) return <Livro livro={livro} />
+          //   else return <Notfound />
+          // }}
+        />
+        <Route component={Notfound} />
+      </Routes>
+      <Rodape />
+    </Router>
+  )
 }
+// }
 
 export default App
